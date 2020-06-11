82b9fbf1d9d8728bf0526b62f60d0851819bc60f
VenkatSanjana <49147780+VenkatSanjana@users.noreply.github.com>
2020-06-11 10:49:33 -0400

    Initial Commit

---
+++ LogGenerator/AWSHelper.cs
@@ -0,0 +1,82 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LogGenerator
+{
+    public class AWSHelper
+    {
+        /// <summary>
+        /// Sends the response to the queue details supplied as parameters
+        /// </summary>
+        /// <param name="responseMessage"></param>
+        /// <param name="responseQueueUrl"></param>
+        /// <param name="responseRegion"></param>
+        /// <returns></returns>
+        public static bool SendResponseToSQSQueue(string accessKey, string secureAccessKey, string responseMessage, string responseQueueUrl, string responseRegion)
+        {
+            try
+            {
+                var sendMessageRequest = new SendMessageRequest
+                {
+                    QueueUrl = responseQueueUrl,
+                    MessageBody = responseMessage
+                };
+                if (responseQueueUrl.ToLower().Contains(".fifo"))
+                {
+                    sendMessageRequest.MessageGroupId = "MessageGroupId";
+                    using (SHA256 hash = SHA256Managed.Create())
+                    {
+                        sendMessageRequest.MessageDeduplicationId
+                            = String.Concat(hash.ComputeHash(Encoding.UTF8.GetBytes(responseMessage)).Select(item => item.ToString("x2")));
+                    }
+                }
+
+                AmazonSQSClient sqsRespClient = new AmazonSQSClient(accessKey, secureAccessKey, RegionEndpoint.GetBySystemName(responseRegion));
+                sqsRespClient.SendMessage(sendMessageRequest);
+                sqsRespClient.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Uploads the response to S3 bucket supplied as parameter
+        /// </summary>
+        /// <param name="keyName"></param>
+        /// <param name="content"></param>
+        /// <param name="bucketName"></param>
+        /// <param name="bucketRegion"></param>
+        /// <returns></returns>
+        public static bool SendResponseToS3Bucket(string accessKey, string secureAccessKey, string keyName, string content, string bucketName, string bucketRegion)
+        {
+            try
+            {
+                PutObjectRequest request = new PutObjectRequest()
+                {
+                    ContentBody = content,
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+                request.Metadata.Add("title", keyName);
+                AmazonS3Client s3Client = new AmazonS3Client(accessKey,
+                    secureAccessKey, RegionEndpoint.GetBySystemName(bucketRegion));
+                PutObjectResponse response = s3Client.PutObject(request);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ LogGenerator/Program.cs
@@ -0,0 +1,158 @@
+﻿using Amazon.S3;
+using Amazon.SQS;
+using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Threading;
+using System.Timers;
+
+namespace LogGenerator
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static System.Timers.Timer timer = new System.Timers.Timer(1000);
+        static Thread thread = null;
+        static int threadCount = 0;
+
+        static void Main(string[] args)
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            log.Info("Application started");
+            timer.Elapsed += ThreadStart;
+            timer.Enabled = true;
+
+            int loopCnt = 1;
+            while (timer.Enabled)
+            {
+                log.Info("Loop count : " + loopCnt.ToString());
+                Thread.Sleep(1000);
+                loopCnt++;
+            }
+
+            log.Info("Application ended");
+        }
+
+        private static void ThreadStart(Object source, ElapsedEventArgs e)
+        {
+            if (thread == null || thread.IsAlive == false)
+            {
+                threadCount++;
+                thread = new Thread(new ThreadStart(Log));
+                thread.Start();
+            }
+            else
+                log.Warn(string.Format("Thread {0} already running..", threadCount.ToString()));
+        }
+
+        private static void Log()
+        {
+            log.Info(string.Format("Thread {0} started..", threadCount.ToString()));
+            switch (threadCount)
+            {
+                case 1:
+                    LogNullReferenceException();
+                    break;
+                case 2:
+                    LogDivideByZeroException();
+                    break;
+                case 3:
+                    LogS3Exception();
+                    break;
+                case 4:
+                    LogSQSException();
+                    break;
+                case 5:
+                    LogFileNotFoundException();
+                    break;
+                case 6:
+                    LogDirectoryNotFoundException();
+                    break;
+                default:
+                    timer.Enabled = false;
+                    break;
+            }
+            Thread.Sleep(2000);
+            log.Info(string.Format("Thread {0} ended..", threadCount.ToString()));
+        }
+
+        private static void LogNullReferenceException()
+        {
+            try
+            {
+                string nullStr = null;
+                nullStr.Contains('a');
+            }
+            catch (NullReferenceException ex)
+            {
+                log.Error(string.Format("Exception occured in LogNullReferenceException() method. Exception message: {0}", ex.Message));
+            }
+        }
+
+        private static void LogDivideByZeroException()
+        {
+            try 
+            {
+                int zero = 0;
+                int num = 1 / zero;
+            }
+            catch (DivideByZeroException ex)
+            {
+                log.Error(string.Format("Exception occured in LogDivideByZeroException() method. Exception message: {0}", ex.Message));
+            }
+        }
+
+        private static void LogSQSException()
+        {
+            try
+            {
+                AWSHelper.SendResponseToSQSQueue("", "", "", "", "");
+            }
+            catch (Exception ex)
+            {
+                log.Error(string.Format("Exception occured in LogSQSException() method. Exception message: {0}", ex.Message));
+            }
+        }
+
+        private static void LogS3Exception()
+        {
+            try
+            {
+                AWSHelper.SendResponseToS3Bucket("", "", "", "", "", "");
+            }
+            catch (Exception ex)
+            {
+                log.Error(string.Format("Exception occured in LogS3Exception() method. Exception message: {0}", ex.Message));
+            }
+        }
+
+        private static void LogFileNotFoundException()
+        {
+            try
+            {
+                File.ReadAllText("abc.txt");
+            }
+            catch (FileNotFoundException ex)
+            {
+                log.Error(string.Format("Exception occured in LogFileNotFoundException() method. Exception message: {0}", ex.Message));
+            }
+        }
+
+        private static void LogDirectoryNotFoundException()
+        {
+            try
+            {
+                Directory.GetFiles("abc");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                log.Error(string.Format("Exception occured in LogDirectoryNotFoundException() method. Exception message: {0}", ex.Message));
+            }
+        }
+    }
+}
